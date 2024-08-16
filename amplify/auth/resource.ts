94b772fff7df965fe988b0170f456982f49194d5
incremental capital <incrementalcapital@gmail.com>
2024-08-16 14:52:53 +0900

    Add Auth UserPool configuration

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,20 @@
 import { defineAuth } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    // Uncomment the following lines to enable additional login options
+    // phone: true,
+    // username: true
   },
+  // Uncomment to customize sign-up fields
+  // signUpAttributes: ['email', 'name'],
+  // Uncomment to customize password policy
+  // passwordPolicyMin: {
+  //   length: 8,
+  //   requireNumbers: true,
+  //   requireSpecialCharacters: true,
+  //   requireLowercase: true,
+  //   requireUppercase: true,
+  // },
 });
