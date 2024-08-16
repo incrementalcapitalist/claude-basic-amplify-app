8c526711253592d4a3e40e3d219e3f5cb0751fbb
incremental capital <incrementalcapital@gmail.com>
2024-08-16 14:27:37 +0900

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
     <Authenticator>
       {({ signOut, user }) => (
         <div>
-          <h1>Hello {user?.username}</h1>
+          <h1>Hello {user.username}</h1>
           <button onClick={signOut}>Sign out</button>
         </div>
       )}
